2cc640836a3c76c662c364050b30a8ab97ea639b
nimrostafarian <adrian2ross@gmail.com>
2022-12-29 15:22:42 +0530

    cmd

--- cmd/mage/main.go
+++ cmd/mage/main.go
@@ -7,7 +7,7 @@ import (
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 
 	"github.com/furya-official/mage/app"
-	"github.com/furya-official/mage/cmd"
+	"github.com/furya-official/mage/cmd/mage"
 )
 
 func main() {
